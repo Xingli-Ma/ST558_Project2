4de11d8bf5da53d2dbb28583caf22c552cbc887c
Nermin Bibic <nbibic@ncsu.edu>
2021-07-09 22:07:35 -0400

    Automate generation of reports

---
+++ Generate_Reports.R
@@ -0,0 +1,21 @@
+library(rmarkdown)
+library(tidyverse)
+
+# Generate report for each day below
+weekdays <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+              "Saturday")
+
+# Numbers corresponding to days; 0 = Sunday, 1 = Monday, etc.
+dayNum <- c(0:6)
+
+# Create file names
+output_file <- paste0(weekdays, "Analysis.md")
+
+# Create a list for each day
+params = lapply(dayNum, FUN = function(x){list(day = x)})
+
+# Put into a data frame
+reports <- tibble(output_file, params)
+
+# Generate reports
+pwalk(reports, render, input = "ST558_Project2.Rmd")
